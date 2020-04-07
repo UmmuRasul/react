0cd55f720fc86e69a7fd2f1804b3054ecd2f1160
UmmuRasul <ummurasul5@gmail.com>
2020-04-07 19:28:01 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render( < ul >
+    <
+    li > 1 < /li> <
+    li > 2 < /li> <
+    li > 3 < /li> < /
+    ul > ,
+    document.getElementById("root")
+)
